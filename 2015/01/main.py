25b5908b6c164876d35416831255e7554247a300
Alain <alain.devcs@gmail.com>
2024-12-25 14:38:56 +0100

    Setup years + 2015/01

---
+++ 2015/01/main.py
@@ -0,0 +1,29 @@
+from utils import *
+
+#########################################################
+#########################################################
+def solve(__file, pt):
+    lines = [line.strip() for line in open(__file)]
+    if len(lines) == 0:
+        return -1
+
+    res = 0
+    for i, line in enumerate(lines):
+        for j, p in enumerate(line):
+            res += 1 if p == "(" else -1
+            if pt == 2:
+                if res == -1:
+                    return j + 1
+
+
+    return res
+#########################################################
+#########################################################
+
+if __name__ == "__main__":
+    input_file = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+    i_sol1 = solve(input_file, 1)
+    print(f"\033[1m\033[92m[SOLUTION] {i_sol1 = }\033[0m")
+    i_sol2 = solve(input_file, 2)
+    print(f"\033[1m\033[92m[SOLUTION] {i_sol2 = }\033[0m")
+
