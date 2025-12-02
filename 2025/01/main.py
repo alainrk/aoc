273f725ced03d6365c0d3272dc4ef8c212dcd0e9
Alain <alain.devcs@gmail.com>
2025-12-02 05:12:43 +0100

    2025/01pt1

---
+++ 2025/01/main.py
@@ -0,0 +1,32 @@
+from utils import *
+
+
+#########################################################
+#########################################################
+@performance
+def solve(__file, pt):
+    lines = [line.strip() for line in open(__file)]
+    if len(lines) == 0:
+        return -1
+
+    res = 0
+    dial = 50
+    for i, line in enumerate(lines):
+        sign, amount = line[:1], int(line[1:])
+        sign = -1 if sign == "L" else 1
+        dial = (dial + (sign * amount)) % 100
+        if dial == 0:
+            res += 1
+
+    return res
+
+
+#########################################################
+#########################################################
+
+if __name__ == "__main__":
+    input_file = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+    i_sol1 = solve(input_file, 1)
+    print(f"\033[1m\033[92m[SOLUTION] {i_sol1 = }\033[0m")
+    i_sol2 = solve(input_file, 2)
+    print(f"\033[1m\033[92m[SOLUTION] {i_sol2 = }\033[0m")
