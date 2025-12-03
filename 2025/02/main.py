48149b4ff89cec0f0d67f6e9348656a2d112b107
Alain <alain.devcs@gmail.com>
2025-12-03 05:47:11 +0100

    2025/02pt2

--- 2025/02/main.py
+++ 2025/02/main.py
@@ -1,6 +1,25 @@
 from utils import *
 
 
+def split_in_chunks(s, n):
+    res = []
+    start, end = 0, n
+    while start < len(s):
+        res.append(s[start:end])
+        start += n
+        end += n
+    return res
+
+
+def all_equal(arr):
+    s = arr[0]
+    for p in arr[1:]:
+        if p != s:
+            return False
+        s = p
+    return True
+
+
 #########################################################
 #########################################################
 @performance
@@ -13,7 +32,7 @@ def solve(__file, pt):
     ranges = []
     for n, line in enumerate(lines):
         chunks = line.split(",")
-        print(chunks)
+        # print(chunks)
         ranges = [list(map(int, x.split("-"))) for x in chunks]
         # Only one line
         break
@@ -21,9 +40,17 @@ def solve(__file, pt):
     for r in ranges:
         for n in range(r[0], r[1] + 1):
             s = str(n)
-            l = len(s) // 2
-            if s[l:] == s[:l]:
-                res += n
+            if pt == 1:
+                l = len(s) // 2
+                if s[l:] == s[:l]:
+                    res += n
+            if pt == 2:
+                for l in reversed(range(1, len(s) // 2 + 1)):
+                    # print("Split:", l, end=", ")
+                    if all_equal(split_in_chunks(s, l)):
+                        # print("Found:", n)
+                        res += n
+                        break
 
     return res
 
@@ -33,7 +60,7 @@ def solve(__file, pt):
 
 if __name__ == "__main__":
     input_file = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
-    i_sol1 = solve(input_file, 1)
-    print(f"\033[1m\033[92m[SOLUTION] {i_sol1 = }\033[0m")
-    # i_sol2 = solve(input_file, 2)
-    # print(f"\033[1m\033[92m[SOLUTION] {i_sol2 = }\033[0m")
+    # i_sol1 = solve(input_file, 1)
+    # print(f"\033[1m\033[92m[SOLUTION] {i_sol1 = }\033[0m")
+    i_sol2 = solve(input_file, 2)
+    print(f"\033[1m\033[92m[SOLUTION] {i_sol2 = }\033[0m")
