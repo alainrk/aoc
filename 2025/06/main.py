390c432de85fa902200ad001038d3fad8cb882b5
Alain <alain.devcs@gmail.com>
2025-12-06 15:10:03 +0100

    2025/06pt2

--- 2025/06/main.py
+++ 2025/06/main.py
@@ -5,7 +5,7 @@ import operator
 #########################################################
 #########################################################
 @performance
-def solve(__file, pt):
+def solve1(__file):
     lines = [line.strip() for line in open(__file)]
     if len(lines) == 0:
         return -1
@@ -30,12 +30,42 @@ def solve(__file, pt):
     return res
 
 
+@performance
+def solve2(__file):
+    lines = [[x for x in line.rstrip("\n")] for line in open(__file)]
+    if len(lines) == 0:
+        return -1
+
+    R, C = len(lines), len(lines[0])
+    res = 0
+    nums = []
+
+    for c in reversed(range(C)):
+        currn = []
+        for r in range(R):
+            char = lines[r][c]
+            if r == R - 1:
+                if len(currn) > 0:
+                    nums.append(int("".join(currn)))
+                    currn = []
+                if char == "+":
+                    res += reduce(operator.add, nums, 0)
+                    nums = []
+                if char == "*":
+                    res += reduce(operator.mul, nums, 1)
+                    nums = []
+                continue
+            if char != " ":
+                currn.append(char)
+    return res
+
+
 #########################################################
 #########################################################
 
 if __name__ == "__main__":
     input_file = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
-    i_sol1 = solve(input_file, 1)
+    i_sol1 = solve1(input_file)
     print(f"\033[1m\033[92m[SOLUTION] {i_sol1 = }\033[0m")
-    # i_sol2 = solve(input_file, 2)
-    # print(f"\033[1m\033[92m[SOLUTION] {i_sol2 = }\033[0m")
+    i_sol2 = solve2(input_file)
+    print(f"\033[1m\033[92m[SOLUTION] {i_sol2 = }\033[0m")
