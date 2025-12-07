93b7063222fc568fe1d264ff7060f4a2d321bcc5
Alain <alain.devcs@gmail.com>
2025-12-07 06:56:36 +0100

    2025/07pt1

--- 2025/07/main.py
+++ 2025/07/main.py
@@ -1,5 +1,6 @@
 from utils import *
 
+
 #########################################################
 #########################################################
 @performance
@@ -9,10 +10,33 @@ def solve(__file, pt):
         return -1
 
     res = 0
+    grid = []
     for i, line in enumerate(lines):
-        pass
+        grid.append([x for x in line])
+
+    R, C = len(lines), len(lines[0])
+    sx, sy = 0, lines[0].index("S")
+
+    beamys = set([sy])
+    for r in range(1, R):
+        new_beamys = deepcopy(beamys)
+        for c, char in enumerate(grid[r]):
+            if char == "^" and c in beamys:
+                res += 1
+                new_beamys.discard(c)
+                left, right = c - 1, c + 1
+                if left >= 0 and left not in new_beamys:
+                    new_beamys.add(left)
+                if r < C and right not in new_beamys:
+                    new_beamys.add(right)
+            else:
+                if c in beamys:
+                    new_beamys.add(c)
+        beamys = new_beamys
 
     return res
+
+
 #########################################################
 #########################################################
 
@@ -20,6 +44,5 @@ if __name__ == "__main__":
     input_file = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
     i_sol1 = solve(input_file, 1)
     print(f"\033[1m\033[92m[SOLUTION] {i_sol1 = }\033[0m")
-    i_sol2 = solve(input_file, 2)
-    print(f"\033[1m\033[92m[SOLUTION] {i_sol2 = }\033[0m")
-
+    # i_sol2 = solve(input_file, 2)
+    # print(f"\033[1m\033[92m[SOLUTION] {i_sol2 = }\033[0m")
